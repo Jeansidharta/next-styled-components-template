c1861e9345a210183404efbb24375c6e2bc7e16e
jean sidharta <jeansidharta@gmail.com>
2020-11-07 08:32:36 -0300

    Added `ecosystem.config.js` from pm2
    I've started to use pm2 in many of my projects. This should make things easier in the future

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+// This is a config file for the process manager `pm2`. If you are not using `pm2`,
+// You can delete this file. For more information about this file, see this link:
+// https://pm2.keymetrics.io/docs/usage/application-declaration/
+
+module.exports = {
+	apps: [{
+		// TODO - add application name here
+		name: 'YOU-APP-NAME-HERE',
+
+		// The serve script will simply serve the contents of the folder specified in the.
+		// arguments
+		script: 'serve',
+
+		// Serves the `out` folder, which is where the site will be staticaly generated.
+		// This folder will be generated when you run `npm run build-export`.
+		args: 'out/',
+
+		// If `watch` is true, a file watcher will be attached to the whole project.
+		// Whenever any file in the project is changed, `pm2` will reload this process.
+		watch: false,
+
+		// Adds timestamp for logs
+		time: true,
+	}],
+};
