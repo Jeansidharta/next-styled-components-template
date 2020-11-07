5e5e974213ff9664e8083e8313559f80da11f54d
jean sidharta <jeansidharta@gmail.com>
2020-11-07 09:21:09 -0300

    Revert "Removed unnecessary global types"
    This reverts commit 66bde2c987680bd2b7a86e1cb3564731639da103.

--- src/global-types.d.ts
+++ src/global-types.d.ts
@@ -1,8 +1,31 @@
 /***************************************************************************
 *                         What is this file?                               *
 *                                                                          *
-* This file declares global types available for the whole project. It's    *
-* useful if you have some typescript helper types.                         *
+* This file declares some global types available for the whole project.    *
+*                                                                          *
 ***************************************************************************/
 
+/**************************** Image-import related types *****************/
+/* This section exists solely to allow for import of image-related types without
+ * the compiler complaining. Don't think too much about it */
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+
+// Other stuff...
 // Empty for the moment. Add new things here...
